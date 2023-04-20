d8f5b9c426c51663c75dc610753cad56409cc5b4
katsuragi484 <diceg.bsak.3saki@gmail.com>
2023-04-20 17:45:00 +0900

    Lesson2 Sample3

---
+++ src/Sample2_3.java
@@ -0,0 +1,7 @@
+public class Sample2_3 {
+    public static void main(String[] args){
+        System.out.println('A');
+        System.out.println("ようこそJavaへ！");
+        System.out.println(123);
+    }
+}
