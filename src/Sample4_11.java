5c6c439cb93f5a44ffb162e7cf9bb71c18e90e82
katsuragi484 <diceg.bsak.3saki@gmail.com>
2023-04-21 19:50:25 +0900

    Lesson4 Sample11

---
+++ src/Sample4_11.java
@@ -0,0 +1,10 @@
+public class Sample4_11 {
+    public static void main(String[] args){
+        int num1 = 5;
+        int num2 = 4;
+
+        double div = (double)num1 / (double)num2;
+
+        System.out.println("5/4は" + div + "です。");
+    }
+}
