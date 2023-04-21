1c37281f6cf208ad12d478fc60ce1858cfa872fd
katsuragi484 <diceg.bsak.3saki@gmail.com>
2023-04-21 17:43:35 +0900

    Lesson4 Sample4

---
+++ src/Sample4_4.java
@@ -0,0 +1,13 @@
+public class Sample4_4 {
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 5;
+
+        System.out.println("num1とnum2に色々な演算を行います。");
+        System.out.println("num1+num2は" + (num1+num2) +"です。");
+        System.out.println("num1-num2は" + (num1-num2) +"です。");
+        System.out.println("num1*num2は" + (num1*num2) +"です。");
+        System.out.println("num1/num2は" + (num1/num2) +"です。");
+        System.out.println("num1%num2は" + (num1%num2) +"です。");
+    }
+}
