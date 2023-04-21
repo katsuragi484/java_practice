54df0d58436da2c9a36887707e6c9133daab36f3
katsuragi484 <diceg.bsak.3saki@gmail.com>
2023-04-21 17:46:42 +0900

    Lesson4 Sample5

---
+++ src/Sample4_5.java
@@ -0,0 +1,12 @@
+public class Sample4_5 {
+    public static void main(String[] args){
+        int a = 0;
+        int b = 0;
+
+        // b = a++;
+        // System.out.println("代入後にインクリメントしたのでbの値は" + b + "です。");
+        
+        b = ++a;
+        System.out.println("代入前にインクリメントしたのでbの値は" + b + "です。");
+    }
+}
