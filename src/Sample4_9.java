cfbb5bd8260709d2f5cdfe7972e91bb140e3c136
katsuragi484 <diceg.bsak.3saki@gmail.com>
2023-04-21 19:43:57 +0900

    Lesson4 Sample9

---
+++ src/Sample4_9.java
@@ -0,0 +1,11 @@
+public class Sample4_9 {
+    public static void main(String[] args) {
+        double dnum = 160.5;
+        System.out.println("身長は" + dnum + "センチです。");
+
+        System.out.println("int型の変数に代入します。");
+
+        int inum = (int)dnum;
+        System.out.println("身長は" + inum + "センチです。");
+    }   
+}
