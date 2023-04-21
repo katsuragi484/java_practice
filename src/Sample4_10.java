c12f73eccb5e2b5d91593d02180234e22d1874c1
katsuragi484 <diceg.bsak.3saki@gmail.com>
2023-04-21 19:45:56 +0900

    Lesson4 Sample10

---
+++ src/Sample4_10.java
@@ -0,0 +1,9 @@
+public class Sample4_10 {
+    public static void main(String[] args){
+        int d = 2;
+        double pi = 3.14;
+
+        System.out.println("直径が" + d + "センチの円の");
+        System.out.println("演習は" + (d*pi) + "センチです。");
+    }
+}
