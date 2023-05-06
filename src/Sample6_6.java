584bc5978bf933d9b43f622547d460dd52097e07
katsuragi484 <diceg.bsak.3saki@gmail.com>
2023-05-06 19:40:32 +0900

    Lesson6 Sample6

---
+++ src/Sample6_6.java
@@ -0,0 +1,12 @@
+public class Sample6_6 {
+ public static void main(String[] args){
+    int i = 1;
+
+    do{
+        System.out.println(i + "番目の繰り返しです。");
+        i++;
+    }while(i<=5);
+
+    System.out.println("繰り返しが終わりました。");
+ }   
+}
