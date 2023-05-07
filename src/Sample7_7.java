ab968966a4678104f5c61fb02c9f07fa0986b5cb
katsuragi484 <diceg.bsak.3saki@gmail.com>
2023-05-07 14:30:57 +0900

    Lesson7 Sample6

---
+++ src/Sample7_7.java
@@ -0,0 +1,11 @@
+public class Sample7_7 {
+    public static void main(String[] args){
+        int[] test = {80,60,22,50,75};
+
+        for(int i=0;i<5;i++){
+            System.out.println((i+1) + "番目の人の点数は" + test[i] + "人です。");
+        }
+
+        System.out.println("テストの受験者は" + test.length + "人です。");
+    }
+}
