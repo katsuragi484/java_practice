122da15d007f4330eef2ab18e4c60730c10b568c
katsuragi484 <diceg.bsak.3saki@gmail.com>
2023-05-07 18:14:44 +0900

    Lesson8 Sample4

---
+++ src/Sample8_4.java
@@ -0,0 +1,25 @@
+class Car{
+    int num;
+    double gas;
+
+    void setNum(int n){
+        num = n;
+        System.out.println("ナンバーを" + num + "にしました。");
+    }
+    void setGas(double g){
+        gas = g;
+        System.out.println("ガソリン量を" + gas + "にしました。");
+    }
+    void show(){
+        System.out.println("車のナンバーは" + num + "です。");
+        System.out.println("ガソリン量は" + gas + "です。");
+    }
+}
+
+public class Sample8_4 {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.setNum(1234);
+        car1.setGas(20.5);
+    }
+}
