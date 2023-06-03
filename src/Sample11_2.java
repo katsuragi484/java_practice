16217bb06fd474557029f4e84f5a33e6a1161111
katsuragi484 <diceg.bsak.3saki@gmail.com>
2023-06-03 11:28:32 +0900

    Lesson11 Sample2

---
+++ src/Sample11_2.java
@@ -0,0 +1,54 @@
+class Car{
+    private int num;
+    private double gas;
+
+    public Car(){
+        num = 0;
+        gas = 0.0;
+        System.out.println("車を作成しました。");
+    }
+
+    public Car(int n, double g){
+        num = n;
+        gas = g;
+        System.out.println("ナンバーを" + num + "にガソリン量を" + gas +"にしました。");
+    }
+
+    public void setCar(int n, double g){
+        num = n;
+        gas = g;
+        System.out.println("ナンバーを" + num + "にガソリン量を" + gas +"にしました。");
+    }
+
+    public void show(){
+        System.out.println("車のナンバーは" + num + "です。");
+        System.out.println("ガソリン量は" + gas + "です。");
+    }
+}
+
+class RacingCar extends Car{
+    private int course;
+    public RacingCar(){
+        course = 0;
+        System.out.println("レーシングカーを作成しました。");
+    }
+
+    public RacingCar(int n, double g, int c){
+        super(n,g);
+        course = c;
+        System.out.println("コース番号" + course + "のレーシングカーを作成しました。");
+    }
+
+    public void setCourse(int c){
+        course = c;
+        System.out.println("コース番号を" + course + "にしました。");
+    }
+}
+
+
+public class Sample11_2 {
+    public static void main(String[] args) {
+        RacingCar rccar1;
+        rccar1 = new RacingCar(1234,20.5,5);
+    }
+}
